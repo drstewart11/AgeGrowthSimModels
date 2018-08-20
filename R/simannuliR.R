85394a112f668abfdddf1826524ef88d4633fd34
David R. Stewart <david_stewart@fws.gov>
2018-08-20 13:47:42 -0600

    Removed params

--- R/simannuliR.R
+++ R/simannuliR.R
@@ -100,7 +100,7 @@ simannuliR<-function(ages,Linf.mu,Linf.cv,k.mu,k.cv,t0.mu,t0.cv,seed){
 
   datR<-cbind(dat.new,annu.mat)
   
-  dat<-gather(datR,ageR,annuR,annu1:colnames(dat[ncol(dat)]),factor_key=TRUE)%>%arrange(id,ageR)
+  dat<-gather(datR,ageR,annuR,annu1:colnames(datR[ncol(datR)]),factor_key=TRUE)%>%arrange(id,ageR)
   str_sub(dat$ageR,start=1,end=4)<-""
   dat%<>%mutate(ageR=as.numeric(ageR))%>%
     filter(!is.na(annuR))%>%
