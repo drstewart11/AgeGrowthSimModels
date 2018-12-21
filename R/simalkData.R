af080c7815e595efdf878efefa057c57d86d1364
David R. Stewart <david_stewart@fws.gov>
2018-12-21 08:00:55 -0700

    Update simalkData.R

--- R/simalkData.R
+++ R/simalkData.R
@@ -33,9 +33,9 @@ simalkData<-function(ages,Linf.mu,Linf.cv,k.mu,k.cv,t0.mu,t0.cv,seed){
     N=length(ages)
     
     #Create empty vector to store Linf, k, and t0 parameter estimates for each individual
-    Linf=rep(0,N)
-    k=rep(0,N)
-    t0=rep(0,N)
+    #Linf=rep(0,N)
+    #k=rep(0,N)
+    #t0=rep(0,N)
     
     #Create dummy params for each individual of fish age a
     make.dummy.inds<-data.frame(
